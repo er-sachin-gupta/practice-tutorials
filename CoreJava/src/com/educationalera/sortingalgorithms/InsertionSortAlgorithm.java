5b630212b64619aec7f66aec3598c38e2e96d936
Sachin Gupta <sgs.engineer@gmail.com>
2017-07-21 17:59:24 +0530

    Commited by Sachin Gupta on 21st July

---
+++ CoreJava/src/com/educationalera/sortingalgorithms/InsertionSortAlgorithm.java
@@ -0,0 +1,46 @@
+package com.educationalera.sortingalgorithms;
+
+/**
+ * Created by Sachin Gupta on 21-07-2017.
+ */
+
+public class InsertionSortAlgorithm {
+
+    public static int[] insertionSortAlgo(int[] array){
+        //Calculation no. of elements in array
+        int j, l = array.length;
+        int temp;
+        for(int i = 1 ; i < l ; i++){
+            temp = array[i];
+            j = i;
+            while(j > 0 && temp < array[j-1]){
+                array[j] = array[j-1];
+                j--;
+            }
+            array[j] = temp;
+        }
+        return array;
+    }
+
+    public static void main(String[] args){
+        int[] unSortedItemArray = {3, 1, 15, -2, 2, 12, 20, 7, 13, 10};
+
+        //Displaying the Unsorted Array Elements
+        System.out.print("Unsorted Array Elements are : ");
+        for(int i = 0 ; i < unSortedItemArray.length ; i++ ){
+            System.out.print(unSortedItemArray[i] + "\t");
+        }
+
+        int[] sortedItemArray = insertionSortAlgo(unSortedItemArray);
+
+        //Displaying the Sorted Array Elements
+        System.out.print("\nSorted Array Elements are : ");
+        /*for(int i = 0 ; i < sortedItemArray.length ; i++ ){
+            System.out.print(sortedItemArray[i] + "\t");
+        }*/
+        for(int arr: sortedItemArray){
+            System.out.print(sortedItemArray[arr] + "\t");
+        }
+
+    }
+}
---
+++ CoreJava/src/com/educationalera/sortingalgorithms/SelectionSortAlgorithm.java
@@ -0,0 +1,56 @@
+package com.educationalera.sortingalgorithms;
+
+/**
+ * Created by Sachin Gupta on 21-07-2017.
+ */
+
+public class SelectionSortAlgorithm {
+
+    //Selection Sort Algorithm
+    public static int[] selectionSortAlgo(int[] Array){
+        int index;
+
+        //Calculating no. of elements in array
+        int l = Array.length;
+
+        for(int i = 0 ; i < l-1; i++){
+            index = i;
+            for(int j = i+1 ; j < l ; j++ ){
+                if(Array[j] < Array[index]){
+                    index = j;
+                }
+            }
+
+            //Exchange Elements
+            swapPositon(index, i, Array);
+        }
+
+        return Array;
+
+    }
+
+    //Method for swapping of Array Elements
+    public static void swapPositon(int smallestNumberIndex, int swappingElementIndex, int[] Array){
+        int temp = Array[smallestNumberIndex];
+        Array[smallestNumberIndex] = Array[swappingElementIndex];
+        Array[swappingElementIndex] = temp;
+    }
+
+    public static void main(String[] args){
+        int[] unSortedItemArray = {3, 1, 15, -2, 2, 12, 20, 7, 13, 10};
+
+        //Displaying the Unsorted Array Elements
+        System.out.print("Unsorted Array Elements are : ");
+        for(int i = 0 ; i < unSortedItemArray.length ; i++ ){
+            System.out.print(unSortedItemArray[i] + "\t");
+        }
+
+        int[] sortedItemArray =  selectionSortAlgo(unSortedItemArray);
+
+        //Displaying the Sorted Array Elements
+        System.out.print("\nSorted Array Elements are : ");
+        for(int i = 0 ; i < sortedItemArray.length ; i++ ){
+            System.out.print(sortedItemArray[i] + "\t");
+        }
+    }
+}
